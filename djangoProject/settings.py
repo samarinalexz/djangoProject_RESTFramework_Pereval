0f9b025c673e1c18e4cf195d7faa66237f5740fb
Алексей Самарин <samarinalexz@gmail.com>
2024-01-14 18:01:43 +0300

    swagger

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -39,9 +39,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_filters',
-    'Pereval'
+    'Pereval',
+    'drf_yasg',
 
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Pereval.urls')),
+    path('docs/', include('rest_framework.urls')),
 ]
+
+urlpatterns += doc_urls
